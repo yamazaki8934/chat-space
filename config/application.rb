de588194b228f93f1915681aed98ad5d4f09e8f7
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-11-04 20:45:25 +0900

    second commit

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.generators do |g|
+        g.test_framework false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
