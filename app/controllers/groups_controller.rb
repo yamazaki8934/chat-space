1f6b38d8a844c9ee1d388dfbdb179359e3832823
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-11-10 20:34:55 +0900

    initial commit

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,7 @@
+class GroupsController < ApplicationController
+  def edit
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
    resources :users, only: [:edit, :update]
    resources :messages, only: [:index, :new]
+   resources :groups, only: [:edit, :new]
   root 'messages#index'
 end
