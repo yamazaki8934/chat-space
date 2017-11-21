94aa8fdf701fc294704b13e9dd5c80f2283f45b9
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-11-21 15:55:38 +0900

    Third commit

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -25,13 +25,13 @@ describe Message do
     it "is invalid without a group_id" do
       message = build(:message, group_id: nil)
       message.valid?
-      expect(message.errors[:group_id]).to include()
+      expect(message.errors[:group]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
       message = build(:message, user_id: nil)
       message.valid?
-      expect(message.errors[:user_id]).to include()
+      expect(message.errors[:user]).to include("を入力してください")
     end
   end
 end
